d5623a6d475d2fa6790ab03b923fe541f2267514
linnefromice <linnefromice@gmail.com>
2024-01-10 20:44:56 +0900

    update

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -1,14 +1,97 @@
+use std::cell::RefCell;
+
+use ic_stable_structures::{DefaultMemoryImpl, memory_manager::{VirtualMemory, MemoryManager, MemoryId}};
+
+mod types;
+
+type MemoryType = VirtualMemory<DefaultMemoryImpl>;
+
+thread_local! {
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+    static STABLE_CELL: RefCell<ic_stable_structures::Cell<types::Snapshot, MemoryType>> = RefCell::new(ic_stable_structures::Cell::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1))), types::Snapshot::default()).unwrap());
+    static STABLE_VEC: RefCell<ic_stable_structures::Vec<types::Snapshot, MemoryType>> = RefCell::new(ic_stable_structures::Vec::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(2)))).unwrap());
+}
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+#[candid::candid_method(query)]
+fn dummy() -> types::Snapshot {
+    types::dummy(ic_cdk::api::time() / (1000 * 1000000))
+}
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn default() -> types::Snapshot {
+    types::Snapshot::default()
+}
+
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn get_last_data() -> types::Snapshot {
+    let datum = STABLE_VEC.with(|mem| {
+        let borrowed_mem = mem.borrow();
+        let len = borrowed_mem.len();
+        let res = borrowed_mem.get(len - 1);
+        res.unwrap().clone()
+    });
+    datum
+}
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+fn get_data(n: u64) -> types::Snapshot {
+    let datum = STABLE_VEC.with(|mem| {
+        let borrowed_mem = mem.borrow();
+        let res = borrowed_mem.get(n as u64);
+        res.unwrap().clone()
+    });
+    datum
+}
+#[ic_cdk::update]
+#[candid::candid_method(update)]
+fn add_from_dummy() {
+    let datum = dummy();
+    add_to_vec(datum);
+}
+#[ic_cdk::update]
+#[candid::candid_method(update)]
+fn add_from_default() {
+    let datum = default();
+    add_to_vec(datum);
+}
+fn add_to_vec(datum: types::Snapshot) {
+    STABLE_VEC.with(|mem| {
+        mem.borrow_mut().push(&datum).unwrap();
+    });
 }
 
 #[cfg(test)]
 mod tests {
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    use candid::{Encode, Decode};
+
+    use super::*;
     candid::export_service!();
 
     #[test]
     fn gen_candid() {
         std::fs::write("interface.did", __export_service()).unwrap();
+    }
+
+    #[test]
+    fn test_default_bytes() {
+        let bytes = Encode!(&default()).unwrap();
+        let data = Decode!(bytes.as_ref(), types::Snapshot).unwrap();
+        let re_bytes = Encode!(&data).unwrap();
+        assert_eq!(bytes, re_bytes);
+    }
+
+    #[test]
+    fn test_dummy_bytes() {
+        let time = SystemTime::now()
+            .duration_since(UNIX_EPOCH).unwrap().as_secs();
+        let bytes = Encode!(&types::dummy(time)).unwrap();
+        let data = Decode!(bytes.as_ref(), types::Snapshot).unwrap();
+        let re_bytes = Encode!(&data).unwrap();
+        assert_eq!(bytes, re_bytes);
     }
 }
---
+++ src/backend/src/types.rs
@@ -0,0 +1,100 @@
+use std::{borrow::Cow, collections::BTreeMap};
+
+use candid::{Decode, Encode};
+
+#[derive(Debug, Clone, Default, candid::CandidType, candid::Deserialize, serde::Serialize)]
+pub struct Snapshot {
+    pub value: SnapshotValue,
+    pub timestamp: u64,
+}
+impl ic_stable_structures::Storable for Snapshot {
+    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+}
+impl ic_stable_structures::BoundedStorable for Snapshot {
+    const MAX_SIZE: u32 = 100;
+    const IS_FIXED_SIZE: bool = false;
+}
+
+#[derive(Debug, Clone, Default, candid::CandidType, candid::Deserialize, serde::Serialize)]
+pub struct SnapshotValue {
+    pub jsonrpc: String,
+    pub id: String,
+    pub result: ResultV3Pool,
+}
+
+#[derive(Debug, Clone, Default, candid::CandidType, candid::Deserialize, serde::Serialize)]
+pub struct ResultV3Pool {
+    pub address: String,
+    pub token0: String,
+    pub sqrt_ratio_x96: String,
+    pub liquidity: String,
+    pub tick_current: i32,
+    pub tick_spacing: i32,
+    #[serde(deserialize_with = "from_ticks")]
+    pub ticks: BTreeMap<String, Tick>
+}
+
+#[derive(Debug, Clone, Default, candid::CandidType, candid::Deserialize, serde::Serialize)]
+pub struct Tick {
+    pub liquidity_gross: String,
+    pub liquidity_net: String,
+    pub fee_growth_outside_0x128: String,
+    pub fee_growth_outside_1x128: String,
+    pub initialized: bool,
+}
+
+// Deserializer
+pub fn from_ticks<'de, D>(deserializer: D) -> Result<BTreeMap<String, Tick>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    deserializer.deserialize_map(CustomVisitor)
+}
+struct CustomVisitor;
+impl<'de> serde::de::Visitor<'de> for CustomVisitor {
+    type Value = BTreeMap<String, Tick>;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(formatter, "error")
+    }
+
+    fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+    where
+        M: serde::de::MapAccess<'de>
+    {
+        let mut result = BTreeMap::new();
+        while let Some((k, v)) = map.next_entry::<i64, Tick>()? {
+            // ic_cdk::println!("key={}", &k);
+            // ic_cdk::println!("value={:?}", &v);
+            result.insert(
+                k.to_string(),
+                v
+            );
+        }
+        Ok(result)
+    }
+}
+
+pub fn dummy(timestamp: u64) -> Snapshot {
+    Snapshot {
+        timestamp,
+        value: SnapshotValue {
+            id: "1".to_string(),
+            jsonrpc: "2.0".to_string(),
+            result: ResultV3Pool {
+                ticks: BTreeMap::new(),
+                tick_current: -77_456,
+                liquidity: "0x1d2f091ff09fb67174738".to_string(),
+                token0: "0x6b175474e89094c44da98b954eedeac495271d0f".to_string(),
+                address: "0xc2e9f25be6257c210d7adf0d4cd6e3e881ba25f8".to_string(),
+                sqrt_ratio_x96: "0x55376cd2ad05b815780ecfb".to_string(),
+                tick_spacing: 60
+            }
+        }
+    }
+}
